ec1037620135dbd878c53391e9fd21c6a73c2ff6
svetak6 <svetak6@gmail.com>
2020-04-23 11:09:49 -0700

    add another solution

--- python/main/find_duplicate.py
+++ python/main/find_duplicate.py
@@ -32,6 +32,16 @@ def find02():
     newDict = {key: value for (key, value) in d.items() if len(value) > 1}
     print('Positions in array: {}'.format(newDict))
 
+def find03(str):
+    lst = list(str)
+    s = set(lst)
+    d = {}
+    for e in s:
+        if ' ' not in e and lst.count(e) > 1:
+            pos = [ i for i,v in enumerate(lst) if e in v]
+            d[e] = pos
+    return d
+
 #newDict = filterTheDict(dictOfNames, lambda elem : elem[0] % 2 == 0)
 # newDict = filterTheDict(dictOfNames, lambda elem: elem[1]>1) requires extra method
 
@@ -39,3 +49,4 @@ def find02():
 find()
 
 find02()
+print(find03('a  a b a b c'))
