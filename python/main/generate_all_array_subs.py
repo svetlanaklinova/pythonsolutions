0fa3f20ecf3d6716faddef64b62717da494ab72f
svetak6 <svetak6@gmail.com>
2020-05-15 11:06:55 -0700

    refactoring existing code

--- python/main/generate_all_array_subs.py
+++ python/main/generate_all_array_subs.py
@@ -1,9 +1,20 @@
-''' Generate and print all subsets of array'''
+''' Generate and print all subsets of array
+input:
+[1,2,3]
+output:
+[[], [2], [3], [1], [1, 2], [1, 3], [2, 3], [1, 2, 3]]
+
+'''
 
 from itertools import chain, combinations
 
 def subset(iterable):
-    lstTuples =  list(chain.from_iterable(set(combinations(iterable, r)) for r in range(len(iterable) + 1)))
+    lstTuples = \
+        list(
+            chain.from_iterable(
+                set(combinations(iterable, r)) for r in range(len(iterable) + 1)
+            )
+        )
     s = [ list(aTuple) for aTuple in lstTuples]
 
     print(s)
@@ -11,4 +22,5 @@ def subset(iterable):
 
 
 assert subset([1,2,3]) == [[], [2], [3], [1], [1, 2], [1, 3], [2, 3], [1, 2, 3]]
+
 
