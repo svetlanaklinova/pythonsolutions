58f7b8dfdf3a393b40832799fc02f1bdc780e51a
svetak6 <svetak6@gmail.com>
2020-05-15 19:38:00 -0700

    fixing

--- python/main/takenTest/stringPatterns.py
+++ python/main/takenTest/stringPatterns.py
@@ -1,61 +1,83 @@
-'''
-Given the length of a word (wordLen) and the maximum number of consecutive vowels that it can contain (maxVowels), determine how many unique words can be generated.
-Words will consist of English alphabetic letters a through z only. Vowels are v: {a, e, i, o, u};
-consonants are c: remaining 21 letters. In the explanations, v and c represent vowels and consonants.
-'''
+
+total_vowels = 5
+total_consonants = 21
+
+def single_pair():
+    global total_consonants,total_vowels
+    ways = 0
+    ways = ways + total_consonants
+    return ways + total_vowels
 
 
-def calculateWays(wordLen, maxVowels):
-    # prevent entering negative values
-    if maxVowels > wordLen:
-        maxVowels = wordLen
-    elif maxVowels < 0:
-        maxVowels = 0
+def consonants_only(wordlen):
+    global total_consonants
+    return total_consonants**wordlen
 
-    if wordLen < 1 :
-        ways = 0
-        print("wordLen {}  {}. Total ways: {}".format(wordLen, maxVowels, ways))
+def vowels_only(maxvowels):
+    global total_vowels
+    return total_vowels**maxvowels
+
+
+def calculateWays( wordlen , maxvowels):
+
+    ways = 0
+    if not wordlen or maxvowels > wordlen:
         return 0
 
 
-    # words with only consonants
-    ways = 0
-    c = 1
-    for i in range(wordLen):
-        c = c * 21
-    ways = ways + c
+    if maxvowels == 0:
+                ways = ways + consonants_only(wordlen)
+
+    elif wordlen == maxvowels:
+                ways = single_pair() ** wordlen
+
+    elif maxvowels < wordlen:
+        # consonants only
+                ways = ways + consonants_only(wordlen)
+
+                #combinations
+                consonants_in_combination = wordlen - maxvowels
+                s_combination = vowels_only(maxvowels) * consonants_only(consonants_in_combination)
+
+                for i in range(wordlen):
+                    ways = ways + s_combination
 
 
-    if maxVowels:
-        if maxVowels == wordLen: # only vowels
-            v = 1
-            for i in range(wordLen):
-                v = v * 5
-                ways = ways + v
 
-            # combinations
-        for i in range(wordLen):
-            c, v = 1, 1
-            for k in range(maxVowels): #vowels one by one
-                v = v * 5
-                for j in range(wordLen - maxVowels): # rest of chars (consonants)
-                    c = c * 21
-                    ways = ways + (v * c)
-
-    print("wordLen {} with maxVowels {}. Total ways: {}".format(wordLen, maxVowels, ways))
+    print(f'wordlen {wordlen} vowels {maxvowels}, result {ways}')
     return ways
 
+### Test ######
+expected = 21
+actual = calculateWays(1, 0)
+print(f'expected {expected}, actual {actual}')
+assert actual == expected #{c} 21
 
-assert calculateWays(-1, -1) == 0
-assert calculateWays(0, 0) == 0
-assert calculateWays(1, 0) == 21 #{c}
-assert calculateWays(1, 1) == 26 #{c, v}
-assert calculateWays(4, 0) == 194481 #{cccc}
-assert calculateWays(4, 1) == 388941 #{cccc,vccc,cvcc,ccvc,cccv}
-print(calculateWays(4, 2)) #{cccc,vccc,cvcc,ccvc,cccv,vvcc,cvvc,ccvv,vcvc,cvcv}
-print(calculateWays(2, 2)) #{cc, vv, cv, vc}
-print(calculateWays(4, 5))
+expected = 21 + 5
+actual = calculateWays(1, 1)
+print(f'expected {expected}, actual {actual}')
+assert calculateWays(1, 1)== expected #{c,v} 21 + 5
+
+expected = 5*5 + 21*21 + 21*5 + 5*21 #676
+actual = calculateWays(2, 2)
+print(f'expected {expected}, actual {actual}')
+assert actual == expected  #{vv,cc,cv,vc} 5*5 + 21*21 + 21*5 + 5*21
+
+expected = 21*21 + 21*5 + 5*21 #651
+actual = calculateWays(2, 1) #{vc,cv,cc}
+print(f'expected {expected}, actual {actual}')
+assert actual == expected
 
 
+expected = 21**4 + 21*21*21*5 + 21*21*5*21 + 21*5*21*21 + 5*21*21*21 #379701
+actual = calculateWays(4, 1) #{cccc,cccv,ccvc,cvcc,vccc}
+print(f'expected {expected}, actual {actual}')
+assert actual == expected
+
+                                                          #
+expected = 21*21*21*21 #194481
+actual = calculateWays(4, 0) #{cccc}
+print(f'expected {expected}, actual {actual}')
+assert actual == expected
 
 
